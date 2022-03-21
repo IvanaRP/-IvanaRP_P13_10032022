7d833ce2106250e6f56f40e81dcead5876b65a2a
IvanaRP <83823351+IvanaRP@users.noreply.github.com>
2022-03-21 14:49:44 +0100

    LoginForm
    added  useState
    add handleSubmit and onChange

--- argent-bank/src/components/LoginForm/index.jsx
+++ argent-bank/src/components/LoginForm/index.jsx
@@ -3,27 +3,47 @@ import "../../style/signin.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
 
+import { useState } from "react";
+
 function LoginForm() {
+  const [userName, setUserName] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    alert("FORM SUBMITED");
+    e.preventDefault();
+  };
+
   return (
     <div className="main bg-dark">
       <div className="sign-in-wrapper">
         <section className="sign-in-content">
           <FontAwesomeIcon icon={faUserCircle}></FontAwesomeIcon>
           <h1>Sign In</h1>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="input-wrapper">
               <label htmlFor="username">Username</label>
-              <input type="text" id="username" />
+              <input
+                type="text"
+                id="username"
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+              />
             </div>
             <div className="input-wrapper">
               <label htmlFor="password">Password</label>
-              <input type="password" id="password" />
+              <input
+                type="password"
+                id="password"
+                value={userPassword}
+                onChange={(e) => setUserPassword(e.target.value)}
+              />
             </div>
             <div className="input-remember">
               <input type="checkbox" id="remember-me" />
               <label htmlFor="remember-me">Remember me</label>
             </div>
-            <button type="button" className="sign-in-button">
+            <button type="subit" className="sign-in-button">
               Sign In
             </button>
           </form>
