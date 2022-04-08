aebf896c8a96082983ec67c099ba0fff4481a913
IvanaRP <83823351+IvanaRP@users.noreply.github.com>
2022-04-08 17:03:33 +0200

    account component
    added account component

--- argent-bank/src/pages/Home/index.jsx
+++ argent-bank/src/pages/Home/index.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import FeaturesHome from '../../components/FeaturesHome'
-import { featuresHome } from '../../data/featuresHome'
-
+import FeaturesHome from "../../components/FeaturesHome";
+import { featuresHome } from "../../data/featuresHome";
 
 import "../../style/home.css";
 
 function Home() {
   return (
-    <div >
+    <div>
       <div className="hero">
         <section className="hero-content">
           <h2 className="sr-only">Promoted Content</h2>
@@ -18,52 +17,17 @@ function Home() {
         </section>
       </div>
       <div className="features">
-                <h2 className="sr-only">Features</h2>
-                {
-                    featuresHome && featuresHome.map((card) => (
-                        <FeaturesHome 
-                            key = {card.id}
-                            feature = {card.name}
-                            title = {card.title}
-                            text = {card.text}
-                        />
-                    ))
-                }
-            </div>
-      {/* <section className="features">
         <h2 className="sr-only">Features</h2>
-        <div className="feature-item">
-          <img src={ChatIconIllustration} alt="Chat Icon" className="feature-icon" />
-          <h3 className="feature-item-title">You are our #1 priority</h3>
-          <p>
-            Need to talk to a representative? You can get in touch through our
-            24/7 chat or through a phone call in less than 5 minutes.
-          </p>
-        </div>
-        <div className="feature-item">
-          <img
-            src={MoneyIconIllustration}
-            alt="Chat Icon"
-            className="feature-icon"
-          />
-          <h3 className="feature-item-title">More savings means higher rates</h3>
-          <p>
-            The more you save with us, the higher your interest rate will be!
-          </p>
-        </div>
-        <div className="feature-item">
-          <img
-            src={SecurityIconIllustration}
-            alt="Chat Icon"
-            className="feature-icon"
-          />
-          <h3 className="feature-item-title">Security you can trust</h3>
-          <p>
-            We use top of the line encryption to make sure your data and money
-            is always safe.
-          </p>
-        </div>
-      </section> */}
+        {featuresHome &&
+          featuresHome.map((card) => (
+            <FeaturesHome
+              key={card.id}
+              feature={card.name}
+              title={card.title}
+              text={card.text}
+            />
+          ))}
+      </div>
     </div>
   );
 }
--- argent-bank/src/pages/Profile/index.jsx
+++ argent-bank/src/pages/Profile/index.jsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Navigate } from "react-router";
 
+import { accounts } from "../../data/accounts";
+import Account from "../../accounts";
+
 // API calls
 import { editUser } from "../../getApi/getApi";
 import "../../style/profil.css";
@@ -86,37 +89,19 @@ function Profile() {
             value="Edit Name"
           />
         )}
-        <h2 className="sr-only">Accounts</h2>
-        <section className="account">
-          <div className="account-content-wrapper">
-            <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-            <p className="account-amount">$2,082.79</p>
-            <p className="account-amount-description">Available Balance</p>
-          </div>
-          <div className="account-content-wrapper cta">
-            <button className="transaction-button">View transactions</button>
-          </div>
-        </section>
-        <section className="account">
-          <div className="account-content-wrapper">
-            <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-            <p className="account-amount">$10,928.42</p>
-            <p className="account-amount-description">Available Balance</p>
-          </div>
-          <div className="account-content-wrapper cta">
-            <button className="transaction-button">View transactions</button>
-          </div>
-        </section>
-        <section className="account">
-          <div className="account-content-wrapper">
-            <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-            <p className="account-amount">$184.30</p>
-            <p className="account-amount-description">Current Balance</p>
-          </div>
-          <div className="account-content-wrapper cta">
-            <button className="transaction-button">View transactions</button>
-          </div>
-        </section>
+
+        <div className="accounts">
+          <h2 className="sr-only">Accounts</h2>
+          {accounts &&
+            accounts.map((account) => (
+              <Account
+                key={account.id}
+                title={account.title}
+                amount={account.amount}
+                description={account.description}
+              />
+            ))}
+        </div>
       </main>
     </div>
   );
