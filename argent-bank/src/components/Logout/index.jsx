455eadf63334c6e15a6068be7cfcb38ae08efe61
IvanaRP <83823351+IvanaRP@users.noreply.github.com>
2023-02-24 17:59:13 +0100

    update

--- argent-bank/src/components/Logout/index.jsx
+++ argent-bank/src/components/Logout/index.jsx
@@ -1,31 +1,31 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { logout, selectUser } from "../../features/login";
+// import React from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { logout, selectUser } from "../../features/login";
 
-/**
- * Logout component to display user's profile page
- * User name
- * Button for logout
- *
- */
+// /**
+//  * Logout component to display user's profile page
+//  * User name
+//  * Button for logout
+//  *
+//  */
 
-function Logout() {
-  const user = useSelector(selectUser);
-  const dispatch = useDispatch();
+// function Logout() {
+//   const user = useSelector(selectUser);
+//   const dispatch = useDispatch();
 
-  const handleLogout = (e) => {
-    // e.preventDefaul();
-    dispatch(logout());
-  };
+//   const handleLogout = (e) => {
+//     // e.preventDefaul();
+//     dispatch(logout());
+//   };
 
-  return (
-    <div className="main bg-dark">
-      <h1>
-        Welcome <span>{user.name}</span>
-      </h1>
-      <button onClick={(e) => handleLogout(e)}> logout</button>
-    </div>
-  );
-}
+//   return (
+//     <div className="main bg-dark">
+//       <h1>
+//         Welcome <span>{user.name}</span>
+//       </h1>
+//       <button onClick={(e) => handleLogout(e)}> logout</button>
+//     </div>
+//   );
+// }
 
-export default Logout;
+// export default Logout;
